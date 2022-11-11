6bfc29803b3d753c691c351c8d22a1692ec363d4
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2022-11-11 09:45:24 +0200

    adding consumer model

--- TicketProcessor.Microservice/Startup.cs
+++ TicketProcessor.Microservice/Startup.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketProcessor.Microservice.Consumers;
 
 namespace TicketProcessor.Microservice
 {
@@ -26,6 +28,34 @@ namespace TicketProcessor.Microservice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+     
+            //services.Configure<MassTransitHostOptions>(options =>
+            //{
+            //    options.WaitUntilStarted = true;
+            //    options.StartTimeout = TimeSpan.FromSeconds(30);
+            //    options.StopTimeout = TimeSpan.FromMinutes(1);
+            //});
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<TicketConsumer>();
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                 
+                    cfg.Host(new Uri("rabbitmq://localhost"), h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cfg.ReceiveEndpoint("ticketQueue", ep =>
+                    {
+                        ep.PrefetchCount = 16;
+                        ep.UseMessageRetry(r => r.Interval(2, 100));
+                        ep.ConfigureConsumer<TicketConsumer>(provider);
+                    });
+                }));
+            });
+            //services.AddMassTransitHostedService();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
